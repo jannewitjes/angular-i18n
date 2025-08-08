a247a475b3518495b11621f952d37bf650cfd0ab
jannewitjes <45061820+jannewitjes@users.noreply.github.com>
2025-08-08 11:44:15 +0200

    Internationalization course (maar het werkt niet.. )

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CafeComponent } from './cafe/cafe.component';
 import { CartComponent } from './cart/cart.component';
 import { ShopComponent } from './shop/shop.component';
+import {HeroComponent} from "./hero/hero.component";
 
 export const routes: Routes = [
    { path: "cafe", component: CafeComponent },
    { path: "cart", component: CartComponent },
+   { path: "hero", component: HeroComponent},
    { path: "", component: ShopComponent },
    { path: "shop", redirectTo: "", pathMatch: "full" }
  ];
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
+import { $localize } from "@angular/localize/init";
 
 @Component({
     selector: 'app-hero',
-    template: `
-    <section>
-      <div class="hero">
-        <div class="hero-image"><img src="assets/img/hero-1.png" alt="Image of homemade blueberry pie"/></div>
-      </div>
-      <div class="logo-container">
-        <img src="assets/img/logomark.png" alt="Logomark for Bethany's Pies" />
-      </div>
-
-      <div class="focused-message">
-        <h2>Pies for every occasion</h2>
-        <p>Visit our cafe or ship to your home</p>
-      </div>
-    </section>
-  `,
+    templateUrl: './hero.component.html',
     styleUrls: ['./hero.component.scss'],
     standalone: true
 })
-export class HeroComponent { }
+export class HeroComponent {
+  componentText = $localize `translate menu text for example`;
+}
